8d78383aac5f62840927ac6b652b78d4aa6add42
MicahMaphet <93409262+MicahMaphet@users.noreply.github.com>
2023-04-08 08:15:41 -0400

    added button and working on data requesting

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ win = tk.Tk()
 win.title("Scouting Data Scraper")
 win.geometry("500x400+50+50")
 
-compBox = tk.Text(win)
+compBox = tk.Text(win, width=20, height=10)
+compBox.place(x=50, y=50)
+def reqData():
+    req = compBox.get("1.0", "end")
+    res = requests.get(req)
+    print(res)
+    
+reqDataButton = tk.Button(win, text="Request Data", command=reqData)
+reqDataButton.place(x=50, y=30)
+
 win.mainloop()
